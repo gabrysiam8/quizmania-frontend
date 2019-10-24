bf87362eeae476929e527a8944ee4f9a413919d7
gabrysiam8 <gabrysiamiedlar@o2.pl>
2019-10-24 11:27:43 +0200

    Add loading spinner to verification page

--- src/components/auth/AccountVerificationPage.js
+++ src/components/auth/AccountVerificationPage.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import API from '../../utils/API';
 import qs from 'qs';
-import {Alert} from 'react-bootstrap';
+import { Alert, Spinner } from 'react-bootstrap';
 
 export class AccountVerificationPage extends Component {
     constructor(props) {
         super(props);
     
         this.state = {
-            confirmed: false,
+            waiting: true,
+            confirmed: "",
             message: ""
         };
 
@@ -28,25 +29,35 @@ export class AccountVerificationPage extends Component {
             })
             .catch(err => {
                 this.setState({
+                    confirmed: false,
                     message: err.response.data
+                });
+            }, () => {
+                this.setState({
+                    waiting: false
                 });
             });
         }
     }
 
     render() {
+        var alert = this.state.confirmed ?
+        <Alert variant="success">
+            <Alert.Heading>Account was successfully verified!</Alert.Heading>
+            <p>To log in to the application click <Alert.Link href="/login">here</Alert.Link>.</p>
+        </Alert>
+        :
+        <Alert variant="danger">
+            <Alert.Heading>Failed to verify e-mail address!</Alert.Heading>
+            <p>{this.state.message}</p>
+        </Alert>;
+
         return (
             <div className="AccountConfirmationPage">
-                {this.state.confirmed ?
-                    <Alert variant="success">
-                        <Alert.Heading>Account was successfully verified!</Alert.Heading>
-                        <p>To log in to the application click <Alert.Link href="/login">here</Alert.Link>.</p>
-                    </Alert>
+                {this.state.waiting ?
+                    <Spinner animation="border" variant="info" />
                     :
-                    <Alert variant="danger">
-                        <Alert.Heading>Failed to verify e-mail address!</Alert.Heading>
-                        <p>{this.state.message}</p>
-                    </Alert>
+                    alert
                 }
             </div>
         )
